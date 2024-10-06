31b8a84b20feb4d678945cb7d10951ae0c70271a
MatheuscAmaral <matheuscamposdoamaral15@gmail.com>
2024-10-06 13:04:41 -0300

    feat: create tables with models and migrates

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,13 +1,42 @@
 import { FastifyReply, FastifyRequest, FastifyPluginAsync } from "fastify";
 import prisma from "../db"; 
 
+interface Data {
+    id: number
+    name: string
+    email: string
+    password: string 
+    cpf: string
+    cep: number
+    rua: string
+    cidade: string
+    uf: string
+    numero: number
+    bairro: string
+}
+
+
 const userRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get('/users', async (request: FastifyRequest, reply: FastifyReply) => {
         try {
-            const users = await prisma.user.findMany();
+            const users = await prisma.users.findMany();
             reply.status(200).send(users);
         } catch (error) {
             reply.status(500).send({ error: 'Failed to retrieve users' });
+        }
+    });
+
+    fastify.post('/users', async (request: FastifyRequest, reply: FastifyReply) => {
+        const data: Data = request.body as Data;
+
+        try {
+            const user = await prisma.users.create({
+                data: data
+            })
+
+            return reply.status(200).send(user);
+        } catch (error) {
+            reply.status(500).send(error);
         }
     });
 };
